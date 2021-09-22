80ffb28cc675ac62ecbb49392735a40e74c334f1
Yorm-sk <75172876+Yorm-sk@users.noreply.github.com>
2021-09-22 21:02:44 +0300

    Initial commit

---
+++ src/main/java/com/example/anketa/CountServlet.java
@@ -0,0 +1,59 @@
+package com.example.anketa;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "countServlet", value = "/count-servlet")
+public class CountServlet extends HttpServlet {
+    private final int[] countArr = new int[8];
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        String q1 = request.getParameter("quest1");
+        String q2 = request.getParameter("quest2");
+        String q3 = request.getParameter("quest3");
+        if (q1 == null || q2 == null || q3 == null) {
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<p>"+"You didn`t answer on some question"+"</p>");
+            out.println("<a href = \"\\\">" + "Get back" + "</a>");
+            out.println("</body></html>");
+
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            countArr[Integer.parseInt(q1)]++;
+            countArr[Integer.parseInt(q2)]++;
+            countArr[Integer.parseInt(q3)]++;
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            String userName = request.getParameter("name");
+            if (userName == null || userName.equals("")) out.println("<h3>"+"Thank you for participation Anonymous xD!" +"</h3>");
+            else out.println("<h3>"+"Thank you for participation " + request.getParameter("name") +"!" +"</h3>");
+            out.println("<a href = \"\\\">" + "Click here to quiz another person" + "</a>" + "<br>");
+            out.println("<a href = \"\\count-servlet\">" + "Click here to summarise answers" + "</a>");
+            out.println("</body></html>");
+        }
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h3>" + "Answers on question 1:" + "</h3>");
+        out.println("<p>" + "Yes - " + countArr[0] + "</p>");
+        out.println("<p>" + "No - " + countArr[1] + "</p>");
+        out.println("<h3>" + "Answers on question 2:" + "</h3>");
+        out.println("<p>" + "1 - " + countArr[2] + "</p>");
+        out.println("<p>" + "2 - " + countArr[3] + "</p>");
+        out.println("<p>" + "4 - " + countArr[4] + "</p>");
+        out.println("<h3>" + "Answers on question 3:" + "</h3>");
+        out.println("<p>" + "male - " + countArr[5] + "</p>");
+        out.println("<p>" + "female - " + countArr[6] + "</p>");
+        out.println("<p>" + "both - " + countArr[7] + "</p>");
+        out.println("<a href = \"\\\">" + "Quiz another person" + "</a>");
+        out.println("</body></html>");
+    }
+
+}
---
+++ target/Anketa-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,52 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz</title>
+</head>
+<body>
+<h2><%= "Answer following questions, please" %></h2>
+<form action="${pageContext.request.contextPath}/count-servlet" method="POST">
+    Enter your name: <input type="text" name="name"><br>
+    Enter your surname: <input type="text" name="surname"><br>
+    Enter your age: <input type="text" name ="age"><br>
+    <p>Do you like penguins?</p>
+    <div>
+        <input type="radio" id ="q1choice1"
+         name="quest1" value="0">
+        <label for="q1choice1">yes</label>
+        <input type="radio" id ="a1choice2"
+               name="quest1" value="1">
+        <label for="a1choice2">no</label>
+    </div>
+    <p>How many wings they have?</p>
+    <div>
+        <input type="radio" id ="q2choice1"
+               name="quest2" value="2">
+        <label for="q2choice1">1</label>
+        <input type="radio" id ="q2choice2"
+               name="quest2" value="3">
+        <label for="q2choice2">2</label>
+        <input type="radio" id="q2choice3"
+               name="quest2" value="4">
+        <label for="q2choice3">4</label>
+    </div>
+    <p>Who is taking care about egg?</p>
+    <div>
+        <input type="radio" id ="q3choice1"
+               name="quest3" value="5">
+        <label for="q3choice1">male</label>
+        <input type="radio" id ="q3choice2"
+               name="quest3" value="6">
+        <label for="q3choice2">female</label>
+        <input type="radio" id="q3choice3"
+               name="quest3" value="7">
+        <label for="q3choice3">both</label>
+    </div>
+        <br>
+        <input type="submit">
+</form>
+<br>
+<a href="${pageContext.request.contextPath}/count-servlet">Summarise answers</a>
+</body>
+</html>
